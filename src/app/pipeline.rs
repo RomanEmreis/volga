5215789f4543614951abba3b313ba6c60aded5f2
Roman Emreis <Roman_Emreis@epam.com>
2024-11-20 18:27:11 +0400

    Small fixes

--- src/app/pipeline.rs
+++ src/app/pipeline.rs
@@ -7,37 +7,33 @@ use crate::{
     Next
 };
 
-#[cfg(feature = "middleware")]
 pub(crate) struct PipelineBuilder {
+    #[cfg(feature = "middleware")]
     middlewares: Middlewares,
-    endpoints: Endpoints,
-}
-
-#[cfg(not(feature = "middleware"))]
-pub(crate) struct PipelineBuilder {
-    endpoints: Endpoints,
-}
-
-#[cfg(feature = "middleware")]
-pub(crate) struct Pipeline {
-    endpoints: Endpoints,
-    start: Option<Next>,
-}
-
-#[cfg(not(feature = "middleware"))]
-pub(crate) struct Pipeline {
     endpoints: Endpoints
 }
 
-#[cfg(feature = "middleware")]
+pub(crate) struct Pipeline {
+    #[cfg(feature = "middleware")]
+    start: Option<Next>,
+    endpoints: Endpoints
+}
+
 impl PipelineBuilder {
+    #[cfg(feature = "middleware")]
     pub(crate) fn new() -> Self {
         Self {
             middlewares: Middlewares::new(),
             endpoints: Endpoints::new()
         }
     }
-    
+
+    #[cfg(not(feature = "middleware"))]
+    pub(crate) fn new() -> Self {
+        Self { endpoints: Endpoints::new() }
+    }
+
+    #[cfg(feature = "middleware")]
     pub(crate) fn build(self) -> Pipeline {
         let start = self.middlewares.compose();
         Pipeline {
@@ -46,10 +42,17 @@ impl PipelineBuilder {
         }
     }
 
+    #[cfg(not(feature = "middleware"))]
+    pub(crate) fn build(self) -> Pipeline {
+        Pipeline { endpoints: self.endpoints }
+    }
+
+    #[cfg(feature = "middleware")]
     pub(crate) fn has_middleware_pipeline(&self) -> bool {
         self.middlewares.is_empty()
     }
 
+    #[cfg(feature = "middleware")]
     pub(crate) fn middlewares_mut(&mut self) -> &mut Middlewares {
         &mut self.middlewares
     }
@@ -59,36 +62,18 @@ impl PipelineBuilder {
     }
 }
 
-#[cfg(not(feature = "middleware"))]
-impl PipelineBuilder {
-    pub(crate) fn new() -> Self {
-        Self {
-            endpoints: Endpoints::new()
-        }
-    }
-
-    pub(crate) fn build(self) -> Pipeline {
-        Pipeline {
-            endpoints: self.endpoints
-        }
-    }
-
-    pub(crate) fn endpoints_mut(&mut self) -> &mut Endpoints {
-        &mut self.endpoints
-    }
-}
-
-#[cfg(feature = "middleware")]
 impl Pipeline {
-    pub(crate) fn has_middleware_pipeline(&self) -> bool {
-        self.start.is_some()
-    }
-
     #[inline]
     pub(crate) fn endpoints(&self) -> &Endpoints {
         &self.endpoints
     }
+    
+    #[cfg(feature = "middleware")]
+    pub(crate) fn has_middleware_pipeline(&self) -> bool {
+        self.start.is_some()
+    }
 
+    #[cfg(feature = "middleware")]
     pub(crate) async fn execute(&self, ctx: HttpContext) -> HttpResult {
         let next = &self.start;
         if let Some(next) = next {
@@ -97,13 +82,5 @@ impl Pipeline {
         } else {
             ctx.execute().await
         }
-    }
-}
-
-#[cfg(not(feature = "middleware"))]
-impl Pipeline {
-    #[inline]
-    pub(crate) fn endpoints(&self) -> &Endpoints {
-        &self.endpoints
     }
 }
