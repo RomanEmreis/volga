ea49a4aabdd60cbb3e3eee7c8d06bf7bc46ff7fa
Roman Emreis <Roman_Emreis@epam.com>
2024-11-20 17:22:10 +0400

    * Introduced middleware feature that's included by default

--- src/app/pipeline.rs
+++ src/app/pipeline.rs
@@ -1,20 +1,35 @@
-﻿use crate::{
-    app::{middlewares::Middlewares,endpoints::Endpoints},
-    HttpContext, 
-    HttpResult, 
+﻿use crate:: app::endpoints::Endpoints;
+#[cfg(feature = "middleware")]
+use crate::{
+    app::middlewares::Middlewares,
+    HttpResult,
+    HttpContext,
     Next
 };
 
+#[cfg(feature = "middleware")]
 pub(crate) struct PipelineBuilder {
     middlewares: Middlewares,
     endpoints: Endpoints,
 }
 
-pub(crate) struct Pipeline {
+#[cfg(not(feature = "middleware"))]
+pub(crate) struct PipelineBuilder {
     endpoints: Endpoints,
-    start: Next,
 }
 
+#[cfg(feature = "middleware")]
+pub(crate) struct Pipeline {
+    endpoints: Endpoints,
+    start: Option<Next>,
+}
+
+#[cfg(not(feature = "middleware"))]
+pub(crate) struct Pipeline {
+    endpoints: Endpoints
+}
+
+#[cfg(feature = "middleware")]
 impl PipelineBuilder {
     pub(crate) fn new() -> Self {
         Self {
@@ -31,6 +46,10 @@ impl PipelineBuilder {
         }
     }
 
+    pub(crate) fn has_middleware_pipeline(&self) -> bool {
+        self.middlewares.is_empty()
+    }
+
     pub(crate) fn middlewares_mut(&mut self) -> &mut Middlewares {
         &mut self.middlewares
     }
@@ -40,15 +59,51 @@ impl PipelineBuilder {
     }
 }
 
+#[cfg(not(feature = "middleware"))]
+impl PipelineBuilder {
+    pub(crate) fn new() -> Self {
+        Self {
+            endpoints: Endpoints::new()
+        }
+    }
+
+    pub(crate) fn build(self) -> Pipeline {
+        Pipeline {
+            endpoints: self.endpoints
+        }
+    }
+
+    pub(crate) fn endpoints_mut(&mut self) -> &mut Endpoints {
+        &mut self.endpoints
+    }
+}
+
+#[cfg(feature = "middleware")]
 impl Pipeline {
+    pub(crate) fn has_middleware_pipeline(&self) -> bool {
+        self.start.is_some()
+    }
+
     #[inline]
     pub(crate) fn endpoints(&self) -> &Endpoints {
         &self.endpoints
     }
 
-    #[inline]
     pub(crate) async fn execute(&self, ctx: HttpContext) -> HttpResult {
-        let next = self.start.clone();
-        next(ctx).await
+        let next = &self.start;
+        if let Some(next) = next {
+            let next: Next = next.clone();
+            next(ctx).await
+        } else {
+            ctx.execute().await
+        }
+    }
+}
+
+#[cfg(not(feature = "middleware"))]
+impl Pipeline {
+    #[inline]
+    pub(crate) fn endpoints(&self) -> &Endpoints {
+        &self.endpoints
     }
 }
