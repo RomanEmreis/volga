9661432663b1def55b28001773223a00bd02c53e
Roman Emreis <r.emreis@outlook.com>
2025-07-20 17:12:07 +0400

    updated benchmarks for dependency injection and middleware

--- benches/mw.rs
+++ benches/mw.rs
@@ -1,4 +1,4 @@
-﻿use volga::{status, App};
+use volga::App;
 
 use std::hint::black_box;
 use criterion::{criterion_group, criterion_main, Criterion};
@@ -6,10 +6,7 @@ use futures_util::future::join_all;
 use reqwest::Client;
 use tokio::{runtime::Runtime, time::Instant};
 
-use std::{
-    io::Error,
-    time::Duration
-};
+use std::time::Duration;
 
 async fn routing(iters: u64, url: &str) -> Duration {
     #[cfg(all(feature = "http1", not(feature = "http2")))]
@@ -18,14 +15,14 @@ async fn routing(iters: u64, url: &str) -> Duration {
     let client = Client::builder().http2_prior_knowledge().build().unwrap();
 
     let url = format!("http://localhost:7878{url}");
-    
+
     let start = Instant::now();
-    
+
     let requests = (0..iters).map(|_| client.get(&url).send());
     let responses = join_all(requests).await;
-    
+
     let elapsed = start.elapsed();
-    
+
     let failed = responses.iter().filter(|r| r.is_err()).count();
     if failed > 0 {
         eprintln!("failed {failed} requests");
@@ -41,23 +38,19 @@ fn benchmark(c: &mut Criterion) {
                 .with_no_delay()
                 .without_body_limit()
                 .without_greeter();
-            
-            app.map_get("/", || async { "Hello, World!" });
-            app.map_get("/err", || async { Error::other("error") });
-            app.map_err(|err: volga::error::Error| async move { status!(500, err.to_string()) });
-            app.map_fallback(|| async { status!(404) });
+
+            app.map_get("/valid", || async {}).filter(|| async { true });
+            app.map_get("/invalid", || async {}).filter(|| async { false });
+
             _ = app.run().await;
         });
     });
-    
-    c.bench_function("ok", |b| b.iter_custom(
-        |iters| rt.block_on(routing(iters, black_box("/")))
+
+    c.bench_function("valid filter", |b| b.iter_custom(
+        |iters| rt.block_on(routing(iters, black_box("/valid")))
     ));
-    c.bench_function("err", |b| b.iter_custom(
-        |iters| rt.block_on(routing(iters, black_box("/err")))
-    ));
-    c.bench_function("fallback", |b| b.iter_custom(
-        |iters| rt.block_on(routing(iters, black_box("/fall")))
+    c.bench_function("invalid filter", |b| b.iter_custom(
+        |iters| rt.block_on(routing(iters, black_box("/invalid")))
     ));
 }
 
