b767a0e7edb58e51eca70ef6190a1b95bea005fe
Roman <39233213+RomanEmreis@users.noreply.github.com>
2025-12-24 20:04:19 +0400

    Problem details updates (#130)
    * Problem details updates
    * added app,route/group helpers for  registering problem details
    * updates for problem! macro

--- volga/src/error.rs
+++ volga/src/error.rs
@@ -25,7 +25,7 @@ pub use self::{
 };
 
 #[cfg(feature = "problem-details")]
-pub use self::problem::Problem;
+pub use self::problem::{Problem, ProblemDetails};
 
 pub mod handler;
 pub mod fallback;
@@ -201,7 +201,7 @@ impl App {
     /// 
     /// # Example
     /// ```no_run
-    /// use volga::{App, error::Error, status};
+    ///  use volga::{App, error::Error, status};
     /// 
     /// # #[tokio::main]
     /// # async fn main() -> std::io::Result<()> {
