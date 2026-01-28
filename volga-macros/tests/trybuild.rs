7398b4df64a68ede9ca19fd7f2df9efbe3784a8f
Roman Emreis <r.emreis@outlook.com>
2026-01-28 15:50:36 +0400

    turned off trybuild tests on tarpaulin check

--- volga-macros/tests/trybuild.rs
+++ volga-macros/tests/trybuild.rs
@@ -2,6 +2,10 @@
 
 #[test]
 fn ui() {
+    if std::env::var_os("CARGO_TARPAULIN").is_some() {
+        return;
+    }
+    
     let tests = trybuild::TestCases::new();
     tests.pass("tests/ui/http_header_ok.rs");
     tests.compile_fail("tests/ui/http_header_invalid.rs");
