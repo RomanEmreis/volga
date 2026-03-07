a341c989f10f38d76ff65ded6f2dcba536367c1b
Roman Emreis <r.emreis@outlook.com>
2026-03-07 13:30:36 +0400

    adjusted openapi fuzzing

--- fuzz/fuzz_targets/fuzz_openapi_gen.rs
+++ fuzz/fuzz_targets/fuzz_openapi_gen.rs
@@ -2,13 +2,15 @@
 
 use libfuzzer_sys::fuzz_target;
 
-const MAX_LEN: usize = 256;
-
 fuzz_target!(|data: &[u8]| {
-    if data.is_empty() || data.len() > MAX_LEN {
+    if data.is_empty() {
         return;
     }
 
-    let selector = u16::from_le_bytes([data[0], *data.get(1).unwrap_or(&0)]);
+    let selector = u16::from_le_bytes([
+        data[0],
+        *data.get(1).unwrap_or(&0)
+    ]);
+
     volga::fuzzing::fuzz_openapi_gen(selector);
 });
